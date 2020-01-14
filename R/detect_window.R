19efca57d86bcb758af7d3aa063c22977762c913
Ofer Mazor <omazor@users.noreply.github.com>
2020-01-14 13:16:38 -0500

    Add/Improve comments in detect_window() algorithm

--- R/detect_window.R
+++ R/detect_window.R
@@ -17,29 +17,41 @@ detect_window <- function(fvimgl, output, reuse=F){
     ftrs  <- readRDS(paste0(output, "_ftrs.RDS"))
 
   }else{
-    fvimgbw <- thresh(fvimgl, 30, 30, 0.1)
+    # fvimgl: raw fly-view image stack
+    # fvimgbw: Find "bright" pixels (px that are 0.1 greater than the 30x30 boxcar filtered version of the image)
+    boxcar = 30
+    fvimgbw <- thresh(fvimgl, boxcar, boxcar, 0.1)
     writeImage(fvimgl[,,1]/255, file=paste0(output, "_fvimgl.png"))
     writeImage(fvimgbw[,,1], file=paste0(output, "_fvimgbw.png"))
+
+    # fvimgbwc: focus on central circular aperture (100 px)
     centermask <- drawCircle(matrix(0,dim(fvimgl)[1],dim(fvimgl)[2]), dim(fvimgl)[1]/2, dim(fvimgl)[2]/2, 100, col=1, fill=1)
     fvimgbwc <- dipr::ssweep(fvimgbw, centermask, op="*")
     writeImage(fvimgbwc[,,1], file=paste0(output, "_fvimgbwc.png"))
     rm(fvimgbw)
+
+    # fvimgbd: mask of body (use fixed threshold of 200)
     fvimgbd <- (255-fvimgl) > 180
     writeImage(fvimgbd[,,1], file=paste0(output, "_fvimgbd.png"))
+
+    # fvimgbwhd: intersection of fvimgbwc and fvimgbd
     fvimgbwhd <- fvimgbwc * fvimgbd
     writeImage(fvimgbwhd[,,1], file=paste0(output, "_fvimgbwhd.png"))
     rm(fvimgbd)
     rm(fvimgbwc)
+
+
     kern1 <- makeBrush(3, shape="diamond")
     fvimgbwhdo <- opening(fvimgbwhd, kern1)
     rm(fvimgbwhd)
     writeImage(fvimgbwhdo[,,1], file=paste0(output, "_fvimgbwhdo.png"))
-    fvimgbwhdlb <- bwlabel(fvimgbwhdo)
-    fvimgbwbrfh <- fillHull(fvimgbwhdlb)
+    fvimgbwhdlb <- bwlabel(fvimgbwhdo) # assign an integer label to each cluster of connected pixels
+    fvimgbwbrfh <- fillHull(fvimgbwhdlb) # fill in empty pixels withing each cluster
     rm(fvimgbwhdo)
 
     # Calculate object size
     message("Calculating window size")
+    # fvimgbwbrfh: mask for the largest pixel cluster
     ftrs <- dipr::sfeatures(fvimgbwbrfh, fvimgbwbrfh)
     maxobj <- lapply(ftrs, function(x) x[which(x[, 'm.pxs'] == max(x[, 'm.pxs'])),])
     nonmaxobjid <- lapply(ftrs, function(x) which(x[, 'm.pxs'] != max(x[, 'm.pxs'])))
